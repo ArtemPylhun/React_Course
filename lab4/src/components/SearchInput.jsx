7789779ff47f681c43624216fe69132ad0b71a21
Artem Pylhun <artem.pylhun@oa.edu.ua>
2024-09-26 20:02:25 +0300

    upd: revert of styles in lab4

--- lab4/src/components/SearchInput.jsx
+++ lab4/src/components/SearchInput.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-const formStyles = {
-  display: "flex",
-  alignItems: "center",
-  padding: "10px", // Padding around the entire form
-};
-
-const inputStyles = {
-  padding: "10px", // Padding inside the input
-  marginRight: "10px", // Space between input and button
-  borderRadius: "5px",
-  border: "1px solid #ccc",
-};
 
 const SearchInput = ({ filterText, onFilterTextChange }) => {
   return (
-    <form style={formStyles}>
+    <form>
       <input
         type="text"
         placeholder="Search..."
         value={filterText}
         onChange={onFilterTextChange}
-        style={inputStyles}
       />
     </form>
   );
